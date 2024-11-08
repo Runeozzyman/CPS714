77797fb8ded53ab57a54cb9bbb39a45e0e029f72
Jahmil <124407027+Jahswaygo@users.noreply.github.com>
2024-11-08 18:25:21 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Login() {
           </h2>
           <LoginField />
           <button className="w-full py-2 text-white bg-yellow-500 rounded hover:bg-yellow-800 focus:outline-none focus:ring focus:ring-yellow-900">
-            <Link href={"/register"}>Register</Link>
+            <Link href={"/Register"}>Sign Up</Link>
           </button>
           <br />
           <Link href="/api/python">
