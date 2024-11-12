d87c81876e5dad8b7c1563114419a9bf4adcf92d
Jahmil <124407027+Jahswaygo@users.noreply.github.com>
2024-11-11 21:18:00 -0500

    Update LoginField.tsx

--- app/components/LoginField.tsx
+++ app/components/LoginField.tsx
@@ -1,22 +1,23 @@
 "use client";
-import { Fragment, useState } from "react";
+import React, { Fragment, useState } from "react";
 import { useRouter } from "next/navigation";
 import { handleLogin } from "../services/handleLogin"; // Adjust the path as necessary
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // State for toggling password visibility
   const [errorMessage, setErrorMessage] = useState("");
   const router = useRouter();
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    handleLogin(e, username, password, setErrorMessage, router);
+  };
+
   return (
     <Fragment>
-      <form
-        onSubmit={(e) =>
-          handleLogin(e, username, password, setErrorMessage, router)
-        }
-        className="space-y-4"
-      >
+      <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <label
             htmlFor="username"
@@ -40,14 +41,23 @@ export default function Login() {
           >
             Password
           </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:ring-blue-200 text-black"
-            required
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:ring-blue-200 text-black"
+              required
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute inset-y-0 right-0 px-3 py-2 text-sm font-medium text-red-600 focus:outline-none"
+            >
+              {showPassword ? "Hide" : "Show"}
+            </button>
+          </div>
         </div>
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         <button
